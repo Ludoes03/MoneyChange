42868674369a53e4e5d4ce6da84f643b80592c45
Ludovico <ludoescrich@gmail.com>
2017-09-05 18:18:08 -0600

    Change 2

---
+++ MoneyChange/MoneyChange/Interfaces/ILocalize.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace MoneyChange.Interfaces
+{
+    public interface ILocalize
+    {
+        CultureInfo GetCurrentCultureInnfo();
+        void SetLocale(CultureInfo ci);
+    }
+}
